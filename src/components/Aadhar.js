958828fe957a702aeb3ee266b634979de131dc22
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-04-02 23:18:38 +0530

    Changes to Aadhar form

--- src/components/Aadhar.js
+++ src/components/Aadhar.js
@@ -4,23 +4,19 @@ class Aadhar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: '',
-      password: ''
+      firstName: '',
+      lastName: '',
+      gender: 'Female',
+      dob: ''
     };
-    this.handleEmailChange = this.handleEmailChange.bind(this);
-    this.handlePassChange = this.handlePassChange.bind(this);
+
+    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleEmailChange(e) {
+  handleChange(e) {
     this.setState({
-      email: e.target.value
-    });
-  }
-
-  handlePassChange(e) {
-    this.setState({
-      password: e.target.value
+      [e.target.id]: e.target.value
     });
   }
 
@@ -32,32 +28,56 @@ class Aadhar extends Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <div className="form-group">
-          <label htmlFor="email">
-            {'Email'}
-            <input
-              name="email"
-              type="email"
-              placeholder="Email address"
-              value={this.state.email}
-              onChange={this.handleEmailChange}
-              className="form-control"
-            />
-          </label>
-          <div className="form-group">
-            <label htmlFor="password">
-              {'Password'}
-              <input
-                name="password"
-                type="password"
-                placeholder="Password"
-                value={this.state.password}
-                onChange={this.handlePassChange}
-                className="form-control"
-              />
-            </label>
-          </div>
+          <label htmlFor="firstName">{'First name'}</label>
+          <input
+            name="firstName"
+            id="firstName"
+            type="text"
+            placeholder="First name"
+            value={this.state.firstName}
+            onChange={this.handleChange}
+            className="form-control"
+          />
         </div>
-        <input type="submit" className="btn btn-default" value="Submit" />
+        <div className="form-group">
+          <label htmlFor="lastName">{'Last name'}</label>
+          <input
+            name="lastName"
+            id="lastName"
+            type="text"
+            placeholder="Last name"
+            value={this.state.lastName}
+            onChange={this.handleChange}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="gender"> {'Gender'} </label>
+          <select
+            id="gender"
+            value={this.state.gender}
+            onChange={this.handleChange}
+            className="form-control"
+          >
+            <option>{'Male'}</option>
+            <option>{'Female'}</option>
+            <option>{'Other'}</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="gender"> {'Date'} </label>
+          <input
+            id="dob"
+            type="date"
+            value={this.state.dob}
+            onChange={this.handleChange}
+            className="form-control"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-default ">
+          {'Submit'}
+        </button>
       </form>
     );
   }
